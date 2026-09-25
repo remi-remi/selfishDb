139c8db76fdf76237442a4b133a177c38f4456e6
remi-remi <remiveritepro@gmail.com>
2026-09-25 09:10:09 +0200

    wrong default name for exel sheet
    Changes to be committed:
    	modified:   src/services/fileSystem/writeObjectListToXlsx.ts

--- src/services/fileSystem/writeObjectListToXlsx.ts
+++ src/services/fileSystem/writeObjectListToXlsx.ts
@@ -4,6 +4,6 @@ export const writeObjectListToXlsx = (destination: string, dataObjectList: Recor
 
    const worksheet = XLSX.utils.json_to_sheet(dataObjectList);
    const workbook = XLSX.utils.book_new();
-   XLSX.utils.book_append_sheet(workbook, worksheet, "Feuille1");
+   XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet");
    XLSX.writeFile(workbook, destination);
 }
